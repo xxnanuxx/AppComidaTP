aa81a73a5afc4d8946660301f45572f002c1a0c8
xXNanuXx <nanu1429@gmail.com>
2022-11-21 21:45:37 -0300

    Ultimos cambios

--- AppComidaTP/Controllers/AgregarPedidoController.cs
+++ AppComidaTP/Controllers/AgregarPedidoController.cs
@@ -25,18 +25,19 @@ namespace AppComidaTP.Controllers
         public IActionResult AgregarBebida(int Codigo)
         {
             List<Bebida>? listaBebida;
-
-            using (PedidosContext context = new())
-            {
-                Comida? comida = context.Comida.Find(Codigo);
-                if (comida != null)
+            
+             using (PedidosContext context = new())
+             {
+                if (prePedido.Comida == null)
                 {
-                    prePedido.Comida = comida.Nombre;
-                    prePedido.Total += comida.Precio;
+                    Comida? comida = context.Comida.Find(Codigo);
+                    if (comida != null)
+                    {
+                        prePedido.Comida = comida.Nombre;
+                        prePedido.Total += comida.Precio;
+                    }
                 }
-
                 listaBebida = context.Bebida.ToList();
-
             }
             return View(listaBebida);
         }
@@ -48,33 +49,112 @@ namespace AppComidaTP.Controllers
             List<Postre>? listaPostre;
 
             using (PedidosContext context = new())
-            {
-                Bebida? bebida = context.Bebida.Find(Codigo);
-                if (bebida != null)
+            {   
+                if (prePedido.Bebida == null)
                 {
-                    prePedido.Bebida = bebida.Nombre;
-                    prePedido.Total += bebida.Precio;
+                    Bebida? bebida = context.Bebida.Find(Codigo);
+                    if (bebida != null)
+                    {
+                        prePedido.Bebida = bebida.Nombre;
+                        prePedido.Total += bebida.Precio;
+                    }
+                    
                 }
+
                 listaPostre = context.Postre.ToList();
 
             }
             return View(listaPostre);
         }
-
+        [HttpGet]
         public IActionResult AgregarCliente(int Codigo)
         {
 
             using (PedidosContext context = new())
-            {
-                Postre? postre = context.Postre.Find(Codigo);
-                if (postre != null)
+            {   
+                if (prePedido.Postre == null)
                 {
-                    prePedido.Bebida = postre.Nombre;
-                    prePedido.Total += postre.Precio;
+                    Postre? postre = context.Postre.Find(Codigo);
+                    if (postre != null)
+                    {
+                        prePedido.Postre = postre.Nombre;
+                        prePedido.Total += postre.Precio;
+                    }
                 }
             }
+            return View(prePedido);
+        }
+
+        [HttpPost]
+        public IActionResult AgregarCliente(Pedido pedido)
+        {
+
+            using (PedidosContext context = new())
+            {
+                if (pedido.Comida == null && pedido.Bebida == null && pedido.Postre == null)
+                {
+                    return RedirectToAction(nameof(FalloDePedido));
+                } else
+                {
+                    if (pedido.Comida == null)
+                        pedido.Comida = "";
+
+                    if (pedido.Bebida == null)
+                        pedido.Bebida = "";
+
+                    if (pedido.Postre == null)
+                        pedido.Postre = "";
+
+                    context.Pedidos.Add(pedido);
+                    context.SaveChanges();
+                    prePedido = new Pedido();
+                }
+                
+            }
+            return RedirectToAction(nameof(PedidoExitoso));
+        }
+
+        public IActionResult FalloDePedido()
+        {
             return View();
         }
+        public IActionResult PedidoExitoso()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult ListarPedidos()
+        {
+            List<Pedido>? listaPedidos = null;
+           
+            using (PedidosContext context = new())
+            {
+                listaPedidos = context.Pedidos.ToList();
+            }
+
+            return View(listaPedidos);
+        }
+
+        [HttpGet]
+        public IActionResult DeletePedido(int id)
+        {
+            using (PedidosContext context = new())
+            {
+                if (id != 0)
+                {
+                    Pedido? pedido = context.Pedidos.Find(id);
+                    if (pedido != null)
+                    {
+                        context.Pedidos.Remove(pedido);
+                        context.SaveChanges();
+                    }
+                }
+            }
+
+            return RedirectToAction(nameof(ListarPedidos));
+        }
+
 
     }
 }
